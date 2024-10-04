ff712e45195a41b325ac1d923dfa986bd26af253
cheol <cheoloffice4854@gmail.com>
2024-10-04 17:57:35 +0900

    AOP 이전까지 완료

---
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -0,0 +1,28 @@
+package org.example.expert.config;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+class PasswordEncoderTest {
+
+    @InjectMocks
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    void matches_메서드가_정상적으로_동작한다() {
+        // given
+        String rawPassword = "testPassword";
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
+        // when - 파라미터 순서 바뀌면 작동안됨
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
+
+        // then
+        assertTrue(matches);
+    }
+}
